99b3986483a0e66cfcbb97dfdd041341d20e34ae
alimukhtar53 <alimukhtar52@gmail.com>
2023-09-26 21:26:46 +0500

    added lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,56 @@
 "use client";
+import React from "react";
+import { lazy, Suspense } from "react";
 import styled from "styled-components";
 import sedans from "@/app/assets/images/icon-sedans.svg";
 import suvs from "@/app/assets/images/icon-suvs.svg";
 import luxury from "@/app/assets/images/icon-luxury.svg";
-import CardComponent from "@/app/components/CardComponent";
-import Badge from "./components/Badge";
+
+const CardComponent = lazy(() => import("@/app/components/CardComponent"));
+const Badge = lazy(() => import("./components/Badge"));
 
 const Home = () => {
+  const [componentsLoaded, setComponentsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    Promise.all([CardComponent, Badge]).then(() => setComponentsLoaded(true));
+  }, []);
   return (
     <Div>
       <Container>
-        <Badge author={"Ali Mukhtar"} />
-        <CardWrapper className="bg-bright-orange">
-          <CardComponent type={"bright-orange"} icon={sedans}>
-            <H1>Sedans</H1>
-            <Paragraph>
-              Choose a sedan for its affordability and excellent fuel economy.
-              Ideal for cruising in the city or on your next road trip.
-            </Paragraph>
-          </CardComponent>
-          <CardComponent type={"dark-cyan"} icon={suvs}>
-            <H1>SUVs</H1>
-            <Paragraph>
-              Take an SUV for its spacious interior, power, and versatility.
-              Perfect for your next family vacation and off-road adventures.
-            </Paragraph>
-          </CardComponent>
-          <CardComponent type={"very-dark-cyan"} icon={luxury}>
-            <H1>Luxury</H1>
-            <Paragraph>
-              Cruise in the best car brands without the bloated prices. Enjoy
-              the enhanced comfort of a luxury rental and arrive in style.
-            </Paragraph>
-          </CardComponent>
-        </CardWrapper>
+        <Suspense fallback={"loading..."}>
+          {componentsLoaded ? (
+            <>
+              <Badge author={"Ali Mukhtar"} />
+              <CardWrapper className="bg-bright-orange">
+                <CardComponent type={"bright-orange"} icon={sedans}>
+                  <H1>Sedans</H1>
+                  <Paragraph>
+                    Choose a sedan for its affordability and excellent fuel
+                    economy. Ideal for cruising in the city or on your next road
+                    trip.
+                  </Paragraph>
+                </CardComponent>
+                <CardComponent type={"dark-cyan"} icon={suvs}>
+                  <H1>SUVs</H1>
+                  <Paragraph>
+                    Take an SUV for its spacious interior, power, and
+                    versatility. Perfect for your next family vacation and
+                    off-road adventures.
+                  </Paragraph>
+                </CardComponent>
+                <CardComponent type={"very-dark-cyan"} icon={luxury}>
+                  <H1>Luxury</H1>
+                  <Paragraph>
+                    Cruise in the best car brands without the bloated prices.
+                    Enjoy the enhanced comfort of a luxury rental and arrive in
+                    style.
+                  </Paragraph>
+                </CardComponent>
+              </CardWrapper>
+            </>
+          ) : null}
+        </Suspense>
       </Container>
     </Div>
   );
